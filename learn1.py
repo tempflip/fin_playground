9caea0209aa1080811e0ce3e5dd9d6ba600e5743
Peter Tempfli <tempflip@gmail.com>
2016-07-22 10:10:36 +0200

    learn

--- learn1.py
+++ learn1.py
@@ -12,6 +12,48 @@ def read_stock(stock, usecols = ['Date', 'Adj Close']):
 	df = df.rename(columns={'Adj Close' : stock})
 	return df
 
+class Model:
+	def __init__(self):
+		self.model = None
+		self.description = "i'm an untrained model"
+		pass
+
+	def __str__(self):
+		return self.description
+
+	def train(self):
+		pass
+
+	def predict(self, x):
+		pass
+
+class Linear_Model(Model):
+	def __init__(self):
+		Model.__init__(self)
+		pass
+
+	def train(self, training_x_matrix, training_y_matrix, degree=1):
+		self.degree = degree
+		# creating a 1...n space len of the training matrix
+		my_space = np.arange(len(training_x_matrix)).reshape(-1,1) 
+		poly = PolynomialFeatures(degree=self.degree)
+		# polynominal features of the space
+		my_space_features = poly.fit_transform(my_space) 
+
+		# training the model
+		regr = linear_model.LinearRegression()
+		regr.fit(my_space_features, training_y_matrix)
+		self.model = regr
+
+		self.description = "i'm a linear model of {} degree".format(self.degree)
+
+	def predict(self, x):
+		## as it is trained on polynominal features, we need to transform x
+		poly = PolynomialFeatures(degree=self.degree)
+		polynominal_features = poly.fit_transform(x)
+		return self.model.predict(polynominal_features)[0]
+
+
 def run():
 	start_date = '2014-01-01'
 	end_date = '2015-12-31'
@@ -27,27 +69,17 @@ def run():
 	daily = df[1:] / df[:-1].values
 	normalized = df / df.values[1]
 
-
-
-	##### fitting
-	my_space = np.arange(len(normalized)).reshape(-1, 1)
-
-	poly = PolynomialFeatures(degree=10)
-	my_space_features = poly.fit_transform(my_space)
-	regr = linear_model.LinearRegression()
-	regr.fit(my_space_features, normalized['CRM'])
-
-	print regr.get_params()
-
-	
-	### predict
-	normalized['CRM_PREDICT'] = np.array([regr.predict(x)[0] for x in my_space_features]).reshape(-1,1)
-
 	fig, ax1 = plt.subplots(1)
 	normalized['CRM'].plot(ax = ax1)
-	normalized['CRM_PREDICT'].plot(ax = ax1)
+	
+	linear_space = np.arange(len(normalized.index))
 
-
+	for degree in range(20):
+		m = Linear_Model()
+		m.train(normalized.index, normalized['CRM'], degree = degree)
+		prediction_name = 'CRM_PREDICT_{}'.format(degree)
+		normalized[prediction_name] = np.array( [m.predict(x) for x in linear_space])
+		normalized[prediction_name].plot(ax = ax1)
 
 
 	'''
